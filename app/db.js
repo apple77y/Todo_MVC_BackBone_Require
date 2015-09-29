a0746ccdcd3c7c3b6f015635494e096a2189ce3c
apple77y <apple77y@gmail.com>
2015-09-29 17:43:59 +0900

    update: dbURI 바꿈

--- app/db.js
+++ app/db.js
@@ -2,7 +2,7 @@
 
 // connect with mongoDB
 var mongoose = require('mongoose');
-var dbURI = 'mongodb://localhost/todo_backbone_database';
+var dbURI = 'mongodb://localhost/todo_backbone_require_database';
 
 mongoose.connect(dbURI,  function(err) {
     if(!err) {
@@ -11,24 +11,3 @@ mongoose.connect(dbURI,  function(err) {
         console.error('Could not connect to MongoDB!');
     }
 });
-
-// mongoose.connect(dbURI);
-
-// mongoose.connection.on('connected', function() {
-// 	console.error('Connect to MongoDB!');
-// });
-
-// mongoose.connection.on('error', function() {
-// 	console.error('Could not connect to MongoDB!');
-// });
-
-// mongoose.connection.on('disconnected', function() {
-// 	console.error('Disconnect to MongoDB!');
-// });
-
-// process.on('SIGINIT', function() {
-// 	mongoose.connection.close(function() {
-// 		console.log('Application process is going down!');
-// 		process.exit(0);
-// 	});
-// });
